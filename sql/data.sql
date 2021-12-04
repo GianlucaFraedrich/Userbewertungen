fa48620727757d22f06b182cf64ac1b0501a1f65
Gianluca Fraedrich <gianluca.fraedrich@googlemail.com>
2021-12-04 14:53:37 +0100

    html templates angepasst und Datenbankname einheitlich gemacht

---
+++ sql/data.sql
@@ -0,0 +1,6 @@
+USE Userratings;
+INSERT INTO content (ID,Titel) VALUES (1,"UP");
+INSERT INTO content (ID,Titel) VALUES (2,"Inception");
+INSERT INTO content (ID,Titel) VALUES (3,"Matrix");
+INSERT INTO content (ID,Titel) VALUES (4,"Geostorm");
+INSERT INTO content (ID,Titel) VALUES (5,"the ultimate showdown of ultimate destiny");

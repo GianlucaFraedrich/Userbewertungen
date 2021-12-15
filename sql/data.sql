3dca5ac3ddcd075183b449d9927d92735399d398
Jan Steenken <j.steenken@francotyp.com>
2021-12-15 13:03:20 +0100

    SQL Genre Tabelle

--- sql/data.sql
+++ sql/data.sql
@@ -1,6 +1,10 @@
 USE Userratings;
-INSERT INTO content (ID,Titel) VALUES (1,"UP");
-INSERT INTO content (ID,Titel) VALUES (2,"Inception");
-INSERT INTO content (ID,Titel) VALUES (3,"Matrix");
-INSERT INTO content (ID,Titel) VALUES (4,"Geostorm");
-INSERT INTO content (ID,Titel) VALUES (5,"the ultimate showdown of ultimate destiny");
+INSERT INTO content (Titel) VALUES ("UP");
+INSERT INTO content (Titel) VALUES ("Inception");
+INSERT INTO content (Titel) VALUES ("Matrix");
+INSERT INTO content (Titel) VALUES ("Geostorm");
+INSERT INTO content (Titel) VALUES ("the ultimate showdown of ultimate destiny");
+INSERT INTO genre (genre) VALUES ("Comedy");
+INSERT INTO genre (genre) VALUES ("Action");
+INSERT INTO genre (genre) VALUES ("Drama");
+INSERT INTO genre (genre) VALUES ("Animation");
